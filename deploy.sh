50630bf0f18c1ecf70999c31144c6978716c8a8c
Chase Ballard <Chaserayball@gmail.com>
2021-06-29 03:42:54 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-sudo curl https://releases.rancher.com/install-docker/20.10.sh | sh
-sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest
+sudo curl https://releases.rancher.com/install-docker/20.10.sh | sh \
+sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest \
 xdg-open 'https://localhost'
